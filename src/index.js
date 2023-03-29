35e4a8ecf6c38ba0aa031958c9148a8cfb1c8ef2
Aditya Kumar <106439535+AddySoulja@users.noreply.github.com>
2023-03-29 15:23:33 +0530

    Module

--- src/index.js
+++ src/index.js
@@ -3,22 +3,27 @@ const fs = require('fs/promises')
 const myFileWriter = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
+	fs.writeFile(fileName, fileContent);
 }
 
 const myFileReader = async (fileName) => {
 	// write code here
 	// dont chnage function name
+	console.log(fs.readFile(fileName))
 }
 
 
 const myFileUpdater = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
+	fs.writeFile(fileName, fileContent);
+
 }
 
 const myFileDeleter = async (fileName) => {
 	// write code here
 	// dont chnage function name
+	fs.unlink(fileName)
 }
 
 
